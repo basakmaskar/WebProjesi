1dcf3a76866670649ebe4c9c5a2a02b54182c860
Huawei <Huawei@LAPTOP-195JHVVH>
2023-12-18 18:52:30 +0300

    Proje dosyası ekle.

---
+++ api/Context/RepositoryContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api.Context
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+    }
+}
