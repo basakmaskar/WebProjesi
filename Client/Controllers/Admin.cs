1790d8410e7d4f2700eba033893599bb597c17ba
basakmaskar <basakmaskar@gmail.com>
2023-12-22 23:53:54 +0300

    admin paneli işlemleri

---
+++ Client/Controllers/Admin.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    
+        [Authorize(Roles = "Admin")]
+        public class AdminController : Controller
+        {
+        // Admin paneli eylemleri
+        services.AddControllersWithViews(config =>
+{
+        var policy = new AuthorizationPolicyBuilder()
+                  .RequireAuthenticatedUser()
+                  .RequireRole("Admin")
+                  .Build();
+        config.Filters.Add(new AuthorizeFilter(policy));
+});
+
+
+
+        }
+
+
+    }
+
---
+++ Client/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace Client.Models
+{
+    public class Users
+    {
+      
+            public int Id { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+       
+
+    }
+}
